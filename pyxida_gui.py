991c232217d4e67726526c76f61cb64fbd6dda8f
Christos Kormaris <chriskormaris@gmail.com>
2018-04-25 22:52:33 +0300

    Pyxida GUI

--- pyxida_gui.py
+++ pyxida_gui.py
@@ -136,5 +136,15 @@ aboutMenu.add_command(label="About", command=about_window)
 #####
 
 
-root.eval('tk::PlaceWindow %s center' % root.winfo_pathname(root.winfo_id()))
+# center the window on screen
+def center(win):
+    win.update_idletasks()
+    width = win.winfo_width()
+    height = win.winfo_height()
+    x = (win.winfo_screenwidth() // 2) - (width // 2)
+    y = (win.winfo_screenheight() // 2) - (height // 2)
+    win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+
+
+center(root)
 root.mainloop()
